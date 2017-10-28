fe7dc142890c54a9bb6a7616bff62160b2272756
rrbrink <ryanbrinkerhoff@live.com>
2017-10-28 16:41:07 -0600

    webpage

---
+++ Final/10-25-2017/script.js
@@ -0,0 +1,28 @@
+console.log("yo");
+function wow() {
+
+  hmm()
+  console.log("inside yo");
+
+  function hmm() {
+
+    ahhh()
+
+    console.log("inside hmm");
+
+    function ahhh() {
+
+      yoDawg()
+
+      console.log("inside ahhh");
+
+      function yoDawg() {
+
+        console.log("inside yoDawg");
+
+      }
+    }
+  }
+}
+wow()
+console.log("taco");
